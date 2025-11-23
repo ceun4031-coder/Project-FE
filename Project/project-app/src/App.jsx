7a3c49a1eeed15fdd45a6f27955604ca19c427c7
ceun4031-coder <ceun4031@gmail.com>
2025-11-23 17:01:51 +0900

    Initial commit

---
+++ Project/project-app/src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonDemo from "./testStyle/ButtonTest";
+import TokenSystemDemo from "./testStyle/TokenSystem";
+import IndexStyleDemo from './testStyle/indexStyle';
+
+function App() {
+
+   return (
+      <div  className="page-container">
+      <ButtonDemo/>
+      <TokenSystemDemo/>
+    </div>
+  );
+}
+
+export default App
+
